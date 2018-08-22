c26189280353c5eb0d5c39ad342c1b677e9a7dc9
adam-sch <adam.schlabach@gmail.com>
2018-08-22 12:52:22 -0700

    JS structure + tooltipster

---
+++ js/src/main.js
@@ -0,0 +1,15 @@
+(function() {
+  var $ = require('jquery');
+  'use strict';
+
+  // Add additional JS in separate include files and require them with relative paths
+  require('../libs/tooltipster.bundle.min.js');
+
+  $('.cloud-diagram__item').tooltipster({
+    animation: 'fade',
+    interactive: true,
+    theme: 'tooltipster-shadow'
+  });
+
+
+}());
